c637790af6235aa96a04c70566846fa0754a56c7
Phill_Lima <philliplima142@gmail.com>
2022-07-29 18:42:45 +0100

    first commit

---
+++ serve.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening in http://localhost:${port}`);
+});
